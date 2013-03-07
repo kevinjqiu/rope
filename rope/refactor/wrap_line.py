789ff67515965c95616e5fb60cfd9006dd1c14c7
Kevin Qiu <kevin@freshbooks.com>
2013-03-06 23:42:38 -0500

    Preferences and defaults

--- rope/refactor/wrap_line.py
+++ rope/refactor/wrap_line.py
@@ -7,10 +7,11 @@ class WrapLine(object):
         self.project = project
         self.pycore = project.pycore
         self.resource = resource
+        self.indent_size = self.project.prefs.get('indent_size', 4)
 
     def get_changes(self, max_width=None):
         if max_width is None:
-            max_width = self.project.prefs['indent_size']
+            max_width = self.project.prefs.get('max_line_width', 79)
 
         pymodule = self.pycore.resource_to_pyobject(self.resource)
 
@@ -39,7 +40,7 @@ class WrapLine(object):
         names = node.names
         lines = ['from %s import (' % node.module]
 
-        current_line = ' ' * 4  # indent
+        current_line = ' ' * self.indent_size
 
         while len(names) > 0:
             name = names.pop(0)
